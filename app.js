b178ae0e103c5c7e1e446b353740dbc6fb87f22a
geckse <geckse@users.noreply.github.com>
2021-12-26 15:28:03 +0100

    added GET endpoints with parameters to set new Data / resetData Endpoint for resetting persistent cached data / bugfixes

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ try {
   if (fs.existsSync(datafile)) {
     persistentConfig = yaml.safeLoad(fs.readFileSync(datafile, 'utf8'));
   } else {
-    console.log('no data.yml, so we just load default config',e);
+    console.log('no data.yml, so we just load default config');
   }
 } catch (e) {
   console.log('failed loading data.yml',e);
@@ -84,6 +84,19 @@ app.get('/', (req, res) => {
 });
 
 /*
+  Delete saved persistent data
+*/
+app.get('/resetData', (req, res) => {
+  let datafile = './data.yml';
+  if (fs.existsSync(datafile)) {
+    fs.unlinkSync(datafile);
+  } else {
+    console.log('no data.yml, so we don\'t need to delete it');
+  }
+  res.json(defaultConfig);
+});
+
+/*
   Build all the dynamic pathes
 */
 
@@ -110,6 +123,7 @@ function nestedLoopEndpoints(obj, app) {
                   app.post(path+'/'+key, (req, res) => {
                     let opath = (path+'/'+key).replace(/\//g,'.').substring(1);
                     let oldData = _get(config,opath);
+                    let defData = _get(defaultConfig,opath);
                     let newData = req.body;
 
                     let aKeys = Object.keys(oldData).sort();
@@ -128,6 +142,7 @@ function nestedLoopEndpoints(obj, app) {
                   app.put(path+'/'+key, (req, res) => {
                     let opath = (path+'/'+key).replace(/\//g,'.').substring(1);
                     let oldData = _get(config,opath);
+                    let defData = _get(defaultConfig,opath);
                     let newData = req.body;
 
                     let aKeys = Object.keys(oldData).sort();
@@ -146,6 +161,7 @@ function nestedLoopEndpoints(obj, app) {
                   app.patch(path+'/'+key, (req, res) => {
                     let opath = (path+'/'+key).replace(/\//g,'.').substring(1);
                     let oldData = _get(config,opath);
+                    let defData = _get(defaultConfig,opath);
                     let newData = req.body;
 
                     let manipulatedData = oldData;
@@ -165,6 +181,7 @@ function nestedLoopEndpoints(obj, app) {
                   app.delete(path+'/'+key, (req, res) => {
                     let opath = (path+'/'+key).replace(/\//g,'.').substring(1);
                     let oldData = _get(config,opath);
+                    let defData = _get(defaultConfig,opath);
                     let newData = _get(defaultConfig,opath);
 
                     _set(config,opath,newData);
@@ -185,13 +202,55 @@ function nestedLoopEndpoints(obj, app) {
                     app.get(path+'/'+key, (req, res) => {
                       let opath = (path+'/'+key).replace(/\//g,'.').substring(1);
                       let data = _get(config,opath);
-                      return res.json(data);
+                      let defData = _get(defaultConfig,opath);
+                      let newData = req.query.value;
+
+                      console.log(typeof data, newData, typeof newData);
+
+                      if(typeof newData === "undefined") return res.json(data);
+                      if(typeof data === "number"){
+                        newData = parseFloat(newData);
+                        console.log(newData);
+                        if(newData === null || isNaN(newData)) newData = undefined;
+                      }
+                      if(typeof newData !== typeof data) return res.status(400).json({message: 'new data is not the same datatype as old data.'});
+
+                      _set(config,opath,newData);
+                      let settetData = _get(config,opath);
+                      persistentCache();
+                      sendUpdateToDevices();
+
+                      return res.json(newData);
                     });
                     console.log(('Created')+' '+chalk.bold.bgGreen(" GET    ")+' '+chalk.bold(path+'/'+key)+' - expect:'+(typeof value));
+
+                    app.get(path+'/'+key+'/:value', (req, res) => {
+                      let opath = (path+'/'+key).replace(/\//g,'.').substring(1);
+                      let data = _get(config,opath);
+                      let defData = _get(defaultConfig,opath);
+                      let newData = req.params.value;
+  
+                      if(typeof newData === "undefined") return res.json(data);
+  
+                      if(typeof data === "number"){
+                        newData = parseFloat(newData);
+                        if(newData === null || isNaN(newData)) newData = undefined;
+                      }
+                      if(typeof newData !== typeof data) return res.status(400).json({message: 'new data is not the same datatype as old data.'});
+
+                      _set(config,opath,newData);
+                      let settetData = _get(config,opath);
+                      persistentCache();
+                      sendUpdateToDevices();
+
+                      return res.json(newData);
+                    });
+                    console.log(('Created')+' '+chalk.bold.bgGreen(" GET    ")+' '+chalk.bold(path+'/'+key+'/:value')+' - expect:'+(typeof value));
 
                     app.post(path+'/'+key, (req, res) => {
                       let opath = (path+'/'+key).replace(/\//g,'.').substring(1);
                       let oldData = _get(config,opath);
+                      let defData = _get(defaultConfig,opath);
                       let newData = req.body[key];
 
                       if(typeof newData === "undefined") return res.status(400).json({message: 'sent data (POST param: '+key+') to update data.'});
@@ -208,6 +267,7 @@ function nestedLoopEndpoints(obj, app) {
                     app.put(path+'/'+key, (req, res) => {
                       let opath = (path+'/'+key).replace(/\//g,'.').substring(1);
                       let oldData = _get(config,opath);
+                      let defData = _get(defaultConfig,opath);
                       let newData = req.body[key];
 
                       if(typeof newData === "undefined") return res.status(400).json({message: 'sent data (PUT param: '+key+') to update data.'});
@@ -224,6 +284,7 @@ function nestedLoopEndpoints(obj, app) {
                     app.patch(path+'/'+key, (req, res) => {
                       let opath = (path+'/'+key).replace(/\//g,'.').substring(1);
                       let oldData = _get(config,opath);
+                      let defData = _get(defaultConfig,opath);
                       let newData = req.body[key];
 
                       if(typeof newData === "undefined") return res.status(400).json({message: 'sent data (PATCH param: '+key+') to update data.'});
@@ -240,6 +301,7 @@ function nestedLoopEndpoints(obj, app) {
                     app.delete(path+'/'+key, (req, res) => {
                       let opath = (path+'/'+key).replace(/\//g,'.').substring(1);
                       let oldData = _get(config,opath);
+                      let defData = _get(defaultConfig,opath);
                       let newData = _get(defaultConfig,opath);
 
                       _set(config,opath,newData);
